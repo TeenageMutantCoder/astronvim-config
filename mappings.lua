5b78689cbb466061463c96886c40d6eb55c378ab
Stevon Wright <stevonisepic@gmail.com>
2023-11-21 06:03:57 -0500

    Uncomment ctrl-s quick save, add clipboard mappings for system clipboard

--- mappings.lua
+++ mappings.lua
@@ -19,14 +19,33 @@ return {
     -- this is useful for naming menus
     -- ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
     ["<leader>y"] = { name = "Copy" },
+    ["<leader>yt"] = {
+      function()
+        messageText = ''
+
+        if vim.o.clipboard == '' then
+          vim.o.clipboard = 'unnamedplus' -- system clipboard
+          messageText = "Now using system clipboard..."
+        else
+          vim.o.clipboard = '' -- neovim clipboard
+          messageText = "Now using neovim clipboard..."
+        end
+
+        vim.notify(messageText, "info", { title = "Updated active clipboard" })
+      end,
+      desc = "Toggle clipboard",
+    },
     ["<leader>yf"] = { "<cmd>let @*=expand('%:t')<CR>", desc = "Copy filename" },
     ["<leader>yd"] = { "<cmd>let @*=expand('%:p:h')<CR>", desc = "Copy directory path" },
     ["<leader>yp"] = { "<cmd>let @*=expand('%:.')<CR>", desc = "Copy relative file path" },
     ["<leader>yP"] = { "<cmd>let @*=expand('%:p')<CR>", desc = "Copy absolute file path" },
+
     ["<leader>W"] = { "<cmd>noa w<CR>", desc = "Save without formatting" },
   },
+
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
