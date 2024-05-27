c716636cd8866f4f08a173b3ff106090ff9f5c8d
Stevon Wright <stevonisepic@gmail.com>
2024-05-27 01:12:52 -0400

    Comment out cutlass

--- lua/community.lua
+++ lua/community.lua
@@ -12,7 +12,7 @@ return {
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.utility.vim-fetch" },
   { import = "astrocommunity.editing-support.undotree" },
-  { import = "astrocommunity.editing-support.cutlass-nvim" },
+  -- { import = "astrocommunity.editing-support.cutlass-nvim" },
   { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.go" },
